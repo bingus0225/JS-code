0e9160a1255bab602ea9fb9b922a0b062eae68c5
patrick-star-lmao <alexanderholguin330@gmail.com>
2023-06-22 13:06:48 -0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -2,13 +2,13 @@
 var doCoolStuff = function () {
     var currentClassName = document.getElementById ('cool').className = 'cool red'
 
-        // changing class name.
-        if (currentClassName == 'cool') {
-            document.getElementById ('cool').className = 'cool red'
-        } else {
-            document.getElementById ('cool').className = 'cool'
-        }
+    // changing class name
+    if (currentClassName == 'cool') {
+        document.getElementById ('cool').className = 'cool red'
+    } else {
+        document.getElementById ('cool').className = 'cool'
     }
+ }
 
 var sayMyName = function (name) {
     alert('My name is: ' +name)
