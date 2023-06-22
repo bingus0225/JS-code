0cb2316f7d6a6b68c0b9c161f3bfc6cf6174f847
patrick-star-lmao <alexanderholguin330@gmail.com>
2023-06-22 13:27:09 -0500

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,14 @@
 // this function does this.
-var doCoolStuff = function () {
-    var currentClassName = document.getElementById ('cool').className = 'cool red'
+function doCoolStuff() {
+    var currentClassName = document.getElementById('cool').className = 'cool red'
 
     // changing class name
     if (currentClassName == 'cool') {
-    document.getElementById ('cool').className = 'cool red'
+        document.getElementById('cool').className = 'cool red'
     } else {
-    document.getElementById ('cool').className = 'cool'
+        document.getElementById('cool').className = 'cool'
     }
- }
+}
 
 function sayMyName(name) {
     alert('My name is: ' + name)
